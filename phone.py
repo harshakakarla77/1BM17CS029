a4257ba4f07af0821f5bfd2d5d07b8c83ec9011a
harshakakarla77 <37071170+harshakakarla77@users.noreply.github.com>
2019-10-11 10:38:54 +0530

    Add files via upload

---
+++ phone.py
@@ -0,0 +1,26 @@
+class CallDetail():
+    def __init__(self,called_no,called_to,dur,typ):
+        
+
+
+
+class Util:
+    def_init__(self):
+        self.list_of_call_objects=None
+
+def parse_customer(self,list_of_call_string):
+    for i in range(len(list_of_call_string)):
+        li[i]=list_of_call_string[i]
+
+    for i in range(4):
+        l[i]=li[i].split(",")
+
+
+
+
+call1="9990000001,9330000001,23,STD"
+call2="9990000001,9330000002,54,Local"
+call3="9990000001,9330000003,6,ISD"
+
+list_of_call_string=[call1,call2,call3]
+Util().parse_customer(list_of_call_string)
